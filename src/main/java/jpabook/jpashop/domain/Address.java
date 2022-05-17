8709f587b213ca2361e26c4b0b6653238c39f0f4
전민우 <mw.jeon@lifesemantics.kr>
2022-05-17 17:59:34 +0900

    값 타입 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -2,14 +2,19 @@ package jpabook.jpashop.domain;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.util.Objects;
 
 @Data
 @Embeddable
 public class Address {
+
+    @Column(length = 10)
     private String city;
+    @Column(length = 20)
     private String street;
+    @Column(length = 5)
     private String zipcode;
 
     public Address() {
@@ -21,16 +26,21 @@ public class Address {
         this.zipcode = zipcode;
     }
 
+    private String fullAddress() {
+        return getCity() + getStreet() + getZipcode();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Address address = (Address) o;
-        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(city, street, zipcode);
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
+
 }
